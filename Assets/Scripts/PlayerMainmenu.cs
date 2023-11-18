5a3ca9c61da83176a9d2230f4ead6c5b20370b42
Agung Gunawan <agg.nst772@gmail.com>
2023-11-18 15:49:05 +0700

    up

---
+++ Assets/Scripts/PlayerMainmenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMainmenu : MonoBehaviour
+{
+    public int danceIndex;
+
+    public Animator animator;
+
+    private void Start()
+    {
+        animator.SetTrigger(Tags.Dance + danceIndex);
+    }
+}
--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -7,7 +7,8 @@ public class Tags : MonoBehaviour
     //Animations player
     public static readonly string AnimasiMove = "AnimasiMove";
     public static readonly string Jump = "Jump";
-    public static readonly string DoubleJump= "DoubleJump";
+    public static readonly string DoubleJump = "DoubleJump";
+    public static readonly string Dance = "Dance";
 
     //Object Resources
     public static readonly string TextLeaderboard = "TextLeaderboard";
