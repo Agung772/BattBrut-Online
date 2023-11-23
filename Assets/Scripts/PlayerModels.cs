c580b8ffdf5506e884dcb93b9203e91b3c3079d3
Agung Gunawan <agg.nst772@gmail.com>
2023-11-23 21:27:44 +0700

    up

---
+++ Assets/Scripts/PlayerModels.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModels : MonoBehaviour
+{
+    public PlayerModel[] playerModels;
+}
+
+[System.Serializable]
+public class PlayerModel
+{
+    public GameObject model3D;
+    public Material material;
+}
